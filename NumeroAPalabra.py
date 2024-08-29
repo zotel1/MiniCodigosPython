7df2e87ea5e22b22d6585ebddf488098eb852300
zotel1 <zotelsigel@gmail.com>
2024-08-28 23:11:07 -0300

    commit

---
+++ NumeroAPalabra.py
@@ -0,0 +1,18 @@
+#__import__
+
+# Funciones
+
+def numeroAPalabras(numero):
+    """
+    Esta función convierte un número entero de hasta tres dígitos a su representación en palabras.
+
+    Parámetros:
+    número (int): número entero de hasta 3 dígitos a convertir
+
+    Retorno:
+    str: representación en palabras de número
+    """
+
+    
+
+# Programa principal
