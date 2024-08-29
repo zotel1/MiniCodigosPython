a662dae68d5cf7cf164b1dead308aa295e767893
zotel1 <zotelsigel@gmail.com>
2024-08-29 16:17:06 -0300

    update: agregamos casos especiales ej dieciuno es reemplazado por once

--- NumeroAPalabra.py
+++ NumeroAPalabra.py
@@ -2,6 +2,8 @@
 
 # Funciones
 
+#valorNumerico = input(int(f'introduce un numero: '))
+
 def numeroAPalabras(numero):
     """
     Esta función convierte un número entero de hasta tres dígitos a su representación en palabras.
@@ -13,18 +15,31 @@ def numeroAPalabras(numero):
     str: representación en palabras de número
     """
 
+    # Representando los digitos en palabras segun su posicion
     en_palabras = ''
-    unidades = ['', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez']
+    unidades = ['', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve']
 
     decenas = ['', 'dieci', 'veinti', 'treinta y', 'cuarenta y', 'cincuenta y', 'sesenta y', 'setenta y', 'ochenta y', 'noventa y']
 
-    centenas = ['', 'cien', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'schocientos', 'novecientos']
+    centenas = ['', 'ciento', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'schocientos', 'novecientos']
 
+    # COnvirtiendo el número a str asegurando que siempre tenga tres dígitos
+    numero = '0' * (3 - len(str(numero))) + str(numero) #9 ==> 009
+    
+    # Convirtiendo cada dígito a número
+    unidad = int(numero[-1])
+    decena = int(numero[-2])
+    centena = int(numero[-3])
 
-    return en_palabras
+    # Juntando todo
+    en_palabras = '{} {}{}'.format(centenas[centena], decenas[decena], unidades[unidad]).strip()
+
+    # Encargandose de casos especiales
+    en_palabras = en_palabras.replace('dieciuno', 'once')
+    return en_palabras.capitalize()
 
 
 # Programa principal
 # Ejemplo 379 se escribe: Tresciento setenta y nueve
-valor = 379
+valor = 318
 print(numeroAPalabras(valor)) # >>> Trescientos setenta y nueve
