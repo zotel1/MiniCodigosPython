97df5a289f25d02307dc4624fad8d60c1acea2d7
zotel1 <zotelsigel@gmail.com>
2024-08-30 12:21:48 -0300

    update: traemos todos los subtitulos en este caso que contengan el link /entry/

---
+++ WebScr.py
@@ -0,0 +1,12 @@
+import re
+from colorama import Fore
+import requests
+
+website = "https://www.vulnhub.com/"
+
+resultado = requests.get(website)
+content = resultado.text
+
+
+patron = r"/entry/[\w-]*"
+maquinas_repetidas = re.findall(patron, str(content))
